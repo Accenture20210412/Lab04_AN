2d420d570f50a92f161a87de04e0a21cb004e542
Adam Niewiadomski <a.niewiadomski@onet.pl>
2021-04-14 16:13:06 +0200

    ObserverMain uzupełniony o komentarze

--- src/main/java/dp/observer/ObserverMain.java
+++ src/main/java/dp/observer/ObserverMain.java
@@ -3,14 +3,18 @@ package main.java.dp.observer;
 public class ObserverMain {
     public static void main(String[] args) {
 
+        // tworzymy nasze instancje firewalla, loggerutila i riskmanagera
         Firewall firewall = new Firewall();
         LoggerUtil loggerUtil = new LoggerUtil(firewall);
         RiskManager riskManager = new RiskManager(firewall);
 
+        // do firewalla dodajemy subskrybentów
         firewall.attach(loggerUtil);
         firewall.attach(riskManager);
 
+        //uruchamiamy firewalla tj uruchamiamy losowe zdarzenie i informujemy o nim "subskrybentów"
         firewall.startFirewall();
+
 
     }
 }
